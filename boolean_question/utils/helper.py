7db834a188bdbe9937cf1476274eb8afe21e3e81
Saad <46227224+Saadmairaj@users.noreply.github.com>
2021-08-02 01:08:49 +0530

    Fix zip file not remove after unzip

--- boolean_question/utils/helper.py
+++ boolean_question/utils/helper.py
@@ -51,20 +51,24 @@ def set_seed(seed):
         torch.cuda.manual_seed_all(seed)
 
 
-def get_model(overwite=False):
+def get_model(overwrite=False):
     """Returns pretained model path or if not exists 
     downloads te model else raise FileNotFoundError."""
-    relative_path = os.path.join(os.path.abspath(os.path.dirname(
-        os.path.dirname(os.path.dirname(__file__)))), "model")
-    if not os.path.exists(relative_path) or overwite:
+    relative_path = os.path.join(os.path.abspath(
+        os.path.dirname(os.path.dirname(__file__))), "model")
+    if not os.path.exists(relative_path) or overwrite:
         file_url = "https://drive.google.com/u/3/uc?id=1nJh6vjVBO8P0mT0IYAxNYqrPo7Uj2j94"
         filename = relative_path + '.zip'
-        if not os.path.exists(filename) or overwite:
+
+        if not os.path.exists(filename) or overwrite:
             filename = gdown.download(file_url, filename, use_cookies=True)
-            if filename is None:
-                raise RuntimeError("Unable to download the model")
+        
+        if filename is None:
+            raise RuntimeError("Unable to download the model")
         gdown.extractall(filename)
+
         temp_dir = os.path.join(os.path.dirname(relative_path), "__MACOSX")
         if os.path.exists(temp_dir):
-            shutil.rmtree(temp_dir)
+            shutil.rmtree(temp_dir, ignore_errors=True)
+        os.remove(filename)
     return os.path.abspath(relative_path)
